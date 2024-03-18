9ab2c00fcc4de5f937e90214c6df4cbf421f6ca4
CodeByBlazej <codebyblazej@gmail.com>
2024-03-18 19:16:54 +0000

    add function returning random computer choice

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello!");
+function getComputerChoice() {
+  let random = (Math.random() * 10);
+  if(random <= 3) {
+    return 'Rock';
+  } else if (random > 3 && random <= 6) {
+    return 'Paper';
+  } else if (random > 6 && random <= 10) {
+    return 'Scissors';
+  }
+}
+
+console.log(getComputerChoice());
