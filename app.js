abfdffe980b1007f8e45a7e09e8d03548f6e1111
CodeByBlazej <codebyblazej@gmail.com>
2024-03-19 19:21:00 +0000

    add functionality to single round function

--- app.js
+++ app.js
@@ -9,8 +9,34 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt('What do you choose? Paper, Scissors or Rock?');
+
 
 function playRound(playerSelection, computerSelection) {
-  
+  playerSelection = playerSelection.toUpperCase();
+
+  if(playerSelection === 'ROCK' && computerSelection === 'Rock') {
+    return 'You have a draw!';
+  } else if (playerSelection === 'ROCK' && computerSelection === 'Paper') {
+    return 'You Lose! Paper beats Rock';
+  } else if (playerSelection === 'ROCK' && computerSelection === 'Scissors') {
+    return 'You Win! Rock beats Scissors';
+  } else if (playerSelection === 'PAPER' && computerSelection === 'Rock') {
+    return 'You Win! Paper beats Rock';
+  } else if (playerSelection === 'PAPER' && computerSelection === 'Paper') {
+    return 'You have a draw!';
+  } else if (playerSelection === 'PAPER' && computerSelection === 'Scissors') {
+    return 'You Lose! Scissors beats Paper';
+  } else if (playerSelection === 'SCISSORS' && computerSelection === 'Rock') {
+    return 'You Lose! Rock beats Scissors';
+  } else if (playerSelection === 'SCISSORS' && computerSelection === 'Paper') {
+    return 'You Win! Scissors beats Paper';
+  } else if (playerSelection === 'SCISSORS' && computerSelection === 'Scissors') {
+    return 'You have a draw!';
+  }
 }
+
+console.log(playRound(playerSelection, computerSelection));
