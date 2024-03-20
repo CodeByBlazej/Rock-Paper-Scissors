7a0ce7a86377d49688802b4165f163438d6d79c7
CodeByBlazej <codebyblazej@gmail.com>
2024-03-20 22:29:59 +0000

    add playGame function
    It's still not working. I am trying to fugure
    out how to solve the issue of the game playing 5 times and
    returning result and score

--- app.js
+++ app.js
@@ -12,35 +12,51 @@ function getComputerChoice() {
 // console.log(getComputerChoice());
 
 const computerSelection = getComputerChoice();
-const playerSelection = prompt('What do you choose? Paper, Scissors or Rock?');
-
+const playerSelection = prompt('What do you choose? Paper, Scissors or Rock?', '');
+let draw = 'You have a draw!';
+let youWin = 'You Win!';
+let computerWin = 'You Lose!';
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
 
-  
+
 
   if(playerSelection === 'ROCK' && computerSelection === 'Rock') {
-    return 'You have a draw!';
+    return draw;
   } else if (playerSelection === 'ROCK' && computerSelection === 'Paper') {
-    return 'You Lose! Paper beats Rock';
+    return computerWin;
   } else if (playerSelection === 'ROCK' && computerSelection === 'Scissors') {
-    return 'You Win! Rock beats Scissors';
+    return youWin;
   } else if (playerSelection === 'PAPER' && computerSelection === 'Rock') {
-    return 'You Win! Paper beats Rock';
+    return youWin;
   } else if (playerSelection === 'PAPER' && computerSelection === 'Paper') {
-    return 'You have a draw!';
+    return draw;
   } else if (playerSelection === 'PAPER' && computerSelection === 'Scissors') {
-    return 'You Lose! Scissors beats Paper';
+    return computerWin;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Rock') {
-    return 'You Lose! Rock beats Scissors';
+    return computerWin;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Paper') {
-    return 'You Win! Scissors beats Paper';
+    return youWin;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Scissors') {
-    return 'You have a draw!';
+    return draw;
   } else if (playerSelection !== 'SCISSORS' || 'PAPER' || 'ROCK') {
     return alert('You entered the wrong value! Try again');
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+function playGame() {
+  playRound(playerSelection, computerSelection)
+
+  if(draw) {
+    return 'drawwwwww'
+  } else {
+    return 'no'
+  }
+}
+
+console.log(playGame());
+
+
