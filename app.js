03e6aefd51c82c618c67ed89e6bacb6af0e0fbe7
CodeByBlazej <codebyblazej@gmail.com>
2024-03-22 21:38:37 +0000

    add loop to playGame function and checks who won

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const computerSelection = getComputerChoice();
+const playerSelection = prompt('What do you choose? Paper, Scissors or Rock?', '');
+let draw = 'You have a draw!';
+let playerWins = 'You Win!';
+let computerWins = 'You Lose!';
+let computerScore = 0;
+let playerScore = 0;
+
+
 function getComputerChoice() {
   let random = (Math.random() * 10);
   if(random <= 3) {
@@ -9,37 +18,27 @@ function getComputerChoice() {
   }
 }
 
-// console.log(getComputerChoice());
-
-const computerSelection = getComputerChoice();
-const playerSelection = prompt('What do you choose? Paper, Scissors or Rock?', '');
-let draw = 'You have a draw!';
-let playerWins = 'You Win!';
-let computerWins = 'You Lose!';
-let computerScore = 0;
-let playerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
 
 
-
   if(playerSelection === 'ROCK' && computerSelection === 'Rock') {
     return draw;
   } else if (playerSelection === 'ROCK' && computerSelection === 'Paper') {
-    return computerWins + ` Computer Score is ${(++computerScore)}`;
+    return computerWins + ` Computer Score is now ${(++computerScore)}!`;
   } else if (playerSelection === 'ROCK' && computerSelection === 'Scissors') {
-    return playerWins + ` Player Score is ${(++playerScore)}`;
+    return playerWins + ` Player Score is now ${(++playerScore)}!`;
   } else if (playerSelection === 'PAPER' && computerSelection === 'Rock') {
-    return playerWins + ` Player Score is ${(++playerScore)}`;
+    return playerWins + ` Player Score is now ${(++playerScore)}!`;
   } else if (playerSelection === 'PAPER' && computerSelection === 'Paper') {
     return draw;
   } else if (playerSelection === 'PAPER' && computerSelection === 'Scissors') {
-    return computerWins + ` Computer Score is ${(++computerScore)}`;
+    return computerWins + ` Computer score is now ${(++computerScore)}!`;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Rock') {
-    return computerWins + ` Computer Score is ${(++computerScore)}`;
+    return computerWins + ` Computer score is now ${(++computerScore)} !`;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Paper') {
-    return playerWins + ` Player Score is ${(++playerScore)}`;
+    return playerWins + ` Player score is now ${(++playerScore)}!`;
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Scissors') {
     return draw;
   } else if (playerSelection !== 'SCISSORS' || 'PAPER' || 'ROCK') {
@@ -47,39 +46,24 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// console.log(playRound(playerSelection, computerSelection));
 
-// function playGame() {
-//   let counter = 0;
-//   while (counter < 5) {
-//     let round = playRound(playerSelection, computerSelection);
-//     // round;
-//     if(computerScore < 5 && playerScore < 5) {
-//       // round;
-//     } else if (computerScore > playerScore) {
-//       return `You Lose! Computer Score is ${computerScore} and Your Score is ${playerScore}`;
-//     } else if (computerScore < playerScore) {
-//       return `You Win! Your Score is ${playerScore} and Computer Score is ${computerScore}`;
-//     }
-
-//     counter++
-//   }  
-// }
-
-function playGame() {
-  round = playRound(playerSelection, computerSelection);
-  while (round < 1) {
-    round++
-    if(playerScore < 5) {
-      round++
-    } else {
-      return 'end'
-    }
-  }
-    console.log(round); 
+function playGame() {  
+  for(let i = 0; i < 5; i++){
+    const playerSelection = prompt('What do you choose? Paper, Scissors or Rock?', '');
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
   }
 
-console.log(playGame());
-// remember tomorrow about let result and let score!
+  if(computerScore < playerScore) {
+    return 'You Win!'
+  } else if (computerScore > playerScore) {
+    return 'Computer Wins!'
+  } else {
+    return 'You have a draw!'
+  }
+}
+
+playGame();
+
 
 
