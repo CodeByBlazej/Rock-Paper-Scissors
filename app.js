25a5cdb2015aa885b3612541c15577c21667c02a
CodeByBlazej <codebyblazej@gmail.com>
2024-04-09 21:22:57 +0100

    play the game in the window

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const paperBTN = document.querySelector(".paper");
 const rockBTN = document.querySelector(".rock");
+const paperBTN = document.querySelector(".paper");
 const scissorsBTN = document.querySelector(".scissors");
 const para = document.querySelector(".para");
 const computerScr = document.querySelector(".computerScore");
 const playerScr = document.querySelector(".playerScore");
+const playAgainBTN = document.querySelector(".playAgain");
+const winner = document.querySelector(".winner");
 
+playAgainBTN.style.display = 'none';
 
-let draw = 'You have a draw!';
-let playerWins = 'You Win!';
-let computerWins = 'You Lose!';
 let computerScore = 0;
 let playerScore = 0;
 let result = '';
-// let currentComputerScore = para.textContent = `You Lose! Computer Score is now ${(++computerScore)}!`;
-// let currentPlayerScore = ` Player Score is now ${(++playerScore)}!`;
 
 function getComputerChoice() {
   let random = (Math.random() * 10);
@@ -26,6 +24,10 @@ function getComputerChoice() {
   }
 }
 
+// if(playerSelection === 'ROCK' && computerSelection === 'Rock' ||
+
+// ) {
+
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
@@ -34,87 +36,111 @@ function playRound(playerSelection, computerSelection) {
   if(playerSelection === 'ROCK' && computerSelection === 'Rock') {
     result = "Draw!";
 
-    // return draw;
+    
   } else if (playerSelection === 'ROCK' && computerSelection === 'Paper') {
     result = "You Lose!";
     ++computerScore;
 
-    // return computerWins + ` Computer Score is now ${(++computerScore)}!`;
+    
   } else if (playerSelection === 'ROCK' && computerSelection === 'Scissors') {
     result = "You Win!";
     ++playerScore;
 
-    // return playerWins + ` Player Score is now ${(++playerScore)}!`;
+    
   } else if (playerSelection === 'PAPER' && computerSelection === 'Rock') {
     result = "You Win!";
     ++playerScore;
 
-    // return playerWins + ` Player Score is now ${(++playerScore)}!`;
+    
   } else if (playerSelection === 'PAPER' && computerSelection === 'Paper') {
     result = "Draw!";
 
-    // return draw;
+   
   } else if (playerSelection === 'PAPER' && computerSelection === 'Scissors') {
     result = "You Lose!";
     ++computerScore;
 
-    // return computerWins + ` Computer score is now ${(++computerScore)}!`;
+    
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Rock') {
     result = "You Lose!";
     ++computerScore;
 
-    // return computerWins + ` Computer score is now ${(++computerScore)} !`;
+    
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Paper') {
     result = "You Win!";
     ++playerScore;
 
-    // return playerWins + ` Player score is now ${(++playerScore)}!`;
+   
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'Scissors') {
     result = "Draw!";
 
-    // return draw;
+
   } else if (playerSelection !== 'SCISSORS' || 'PAPER' || 'ROCK') {
     return 'You entered the wrong value! Try again';
   }
 }
 
+let checkWinner = function () {
+  if (computerScore === 5 || playerScore === 5) {
+    if (computerScore < playerScore) {
+      winner.textContent = 'You Won The Game!';
+      playAgainBTN.style.display = 'block';
+      paperBTN.disabled = true;
+      rockBTN.disabled = true;
+      scissorsBTN.disabled = true;
 
-// function playGame() {  
-//   for(let i = 0; i < 5; i++){
-//     const playerSelection = prompt('What do you choose? Paper, Scissors or Rock?');
-//     const computerSelection = getComputerChoice();
-//     if(playerSelection === null) {
-//       return 'Please enter correct value!'
-//     } else {
-//       console.log(playRound(playerSelection, computerSelection));
-//     }
-//   }
+    } else if (computerScore > playerScore) {
+      winner.textContent = 'You Lost The Game!';
+      playAgainBTN.style.display = 'block';
+      paperBTN.disabled = true;
+      rockBTN.disabled = true;
+      scissorsBTN.disabled = true;
+    } else {
+      winner.textContent = 'You Had A Draw!';
+      playAgainBTN.style.display = 'block';
+    }
+  } 
+}
 
-//   if(computerScore < playerScore) {
-//     return 'You Win!'
-//   } else if (computerScore > playerScore) {
-//     return 'Computer Wins!'
-//   } else {
-//     return 'You have a draw!'
-//   } 
-// }
 
-// console.log(playGame());
 
 rockBTN.addEventListener('click', () => {
   playRound("rock", getComputerChoice());
   para.textContent = result;
   computerScr.textContent = `Computer score is: ${computerScore}`;
   playerScr.textContent = `Player score is: ${playerScore}`;
+  checkWinner();
 });
 
 paperBTN.addEventListener('click', () => {
-  console.log(playRound("paper", getComputerChoice()));
+  playRound("paper", getComputerChoice());
+  para.textContent = result;
+  computerScr.textContent = `Computer score is: ${computerScore}`;
+  playerScr.textContent = `Player score is: ${playerScore}`;
+  checkWinner();
+
 });
 
 scissorsBTN.addEventListener('click', () => {
-  console.log(playRound("scissors", getComputerChoice()));
+  playRound("scissors", getComputerChoice());
+  para.textContent = result;
+  computerScr.textContent = `Computer score is: ${computerScore}`;
+  playerScr.textContent = `Player score is: ${playerScore}`;
+  checkWinner();
 });
+
+playAgainBTN.addEventListener('click', () => {
+  paperBTN.disabled = false;
+  rockBTN.disabled = false;
+  scissorsBTN.disabled = false;
+  computerScore = 0;
+  playerScore = 0;
+  computerScr.textContent = 'Computer score is: 0';
+  playerScr.textContent = 'Player score is: 0';
+  para.textContent = 'Choose Rock, Paper or Scissors...';
+  winner.textContent = '';
+  playAgainBTN.style.display = 'none';
+})
 
 
 
